33e2f93fbc677fdffffc257947041b54c45a8803
tails <tails0009@gmail.com>
2024-07-08 19:46:39 -0400

    Updated lang file to en_us.json rather than en_US.json The US did not compile to live build

--- src/main/java/com/githubtrulytails/kultscultmod/datagen/AdvancementsProvider.java
+++ src/main/java/com/githubtrulytails/kultscultmod/datagen/AdvancementsProvider.java
@@ -2,14 +2,8 @@ package com.githubtrulytails.kultscultmod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
-import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementEntry;
-import net.minecraft.advancement.AdvancementFrame;
-import net.minecraft.advancement.criterion.InventoryChangedCriterion;
-import net.minecraft.advancement.criterion.OnKilledCriterion;
-import net.minecraft.item.Items;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+
 
 import java.util.function.Consumer;
 
